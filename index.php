45978b32941e4b2a42c751236d589ed232b48e6a
ZaKidTaiwan <33891748+ZaKidTaiwan@users.noreply.github.com>
2017-11-22 20:28:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ $access_token ='Dn4o1ydKqOiRaVD9vjPBqKhKrEATFbyECUyLiBkT2JtyCyAuzIr1NhhrF1iugNGF
 //define('TOKEN', '你的Channel Access Token');
 
 $json_string = file_get_contents('php://input');
+
+$file = fopen("C:\\Line_log.txt", "a+");
+fwrite($file, $json_string."\n"); 
 $json_obj = json_decode($json_string);
 
 $event = $json_obj->{"events"}[0];
@@ -20,11 +23,14 @@ $post_data = [
     ]
   ]
 ]; 
+fwrite($file, json_encode($post_data)."\n");
 
 $ch = curl_init("https://api.line.me/v2/bot/message/reply");
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json',
@@ -32,5 +38,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     //'Authorization: Bearer '. TOKEN
 ));
 $result = curl_exec($ch);
+fwrite($file, $result."\n");  
+fclose($file);
 curl_close($ch); 
 ?>
