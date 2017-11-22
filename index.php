2f9d119931d56d238aa5047a65cd35d3f84683c4
ZaKidTaiwan <33891748+ZaKidTaiwan@users.noreply.github.com>
2017-11-22 20:34:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
-$access_token ='Dn4o1ydKqOiRaVD9vjPBqKhKrEATFbyECUyLiBkT2JtyCyAuzIr1NhhrF1iugNGFMJ+D6vs6uaNeizg3oGO0prSiYxh1y1K8ozA8I9xzCq7Mqa7hwAYcy7THT56yS7aV3c4oI55F6ewwdbPuQtobPwdB04t89/1O/w1cDnyilFU=
-';
+$access_token ='你的Dn4o1ydKqOiRaVD9vjPBqKhKrEATFbyECUyLiBkT2JtyCyAuzIr1NhhrF1iugNGFMJ+D6vs6uaNeizg3oGO0prSiYxh1y1K8ozA8I9xzCq7Mqa7hwAYcy7THT56yS7aV3c4oI55F6ewwdbPuQtobPwdB04t89/1O/w1cDnyilFU=';
 //define('TOKEN', '你的Channel Access Token');
 
 $json_string = file_get_contents('php://input');
-
-$file = fopen("C:\\Line_log.txt", "a+");
-fwrite($file, $json_string."\n"); 
 $json_obj = json_decode($json_string);
 
 $event = $json_obj->{"events"}[0];
@@ -23,14 +19,11 @@ $post_data = [
     ]
   ]
 ]; 
-fwrite($file, json_encode($post_data)."\n");
 
 $ch = curl_init("https://api.line.me/v2/bot/message/reply");
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json',
@@ -38,7 +31,5 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     //'Authorization: Bearer '. TOKEN
 ));
 $result = curl_exec($ch);
-fwrite($file, $result."\n");  
-fclose($file);
 curl_close($ch); 
 ?>
