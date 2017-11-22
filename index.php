56ca8ab399a3c695d25ddeddfe85139cef0e73fb
ZaKidTaiwan <33891748+ZaKidTaiwan@users.noreply.github.com>
2017-11-22 20:25:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-hi
+<?php
+$access_token ='你的Channel Access Token';
+//define('TOKEN', '你的Channel Access Token');
+
+$json_string = file_get_contents('php://input');
+$json_obj = json_decode($json_string);
+
+$event = $json_obj->{"events"}[0];
+$type  = $event->{"message"}->{"type"};
+$message = $event->{"message"};
+$reply_token = $event->{"replyToken"};
+        
+$post_data = [
+  "replyToken" => $reply_token,
+  "messages" => [
+    [
+      "type" => "text",
+      "text" => $message->{"text"}
+    ]
+  ]
+]; 
+
+$ch = curl_init("https://api.line.me/v2/bot/message/reply");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Authorization: Bearer '.$access_token
+    //'Authorization: Bearer '. TOKEN
+));
+$result = curl_exec($ch);
+curl_close($ch); 
+?>
